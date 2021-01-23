c9055de8b28e5f8420f91b8cdc5d440cd0d2c97b
Arne Coucheron <arco68@gmail.com>
2021-01-23 04:01:41 +0100

    Revert "Snap: Remove saving to sdcard"
    This reverts commit ae0a8f6a11d4be8234dbe6708676405f98882c95.
    
    Change-Id: I47335bd39d4b8a96658a14fd51af6c3d581327af

--- src/com/android/camera/multi/MultiCameraModule.java
+++ src/com/android/camera/multi/MultiCameraModule.java
@@ -428,6 +428,11 @@ public class MultiCameraModule implements CameraModule, PhotoController {
     }
 
     @Override
+    public void onSwitchSavePath() {
+
+    }
+
+    @Override
     public void waitingLocationPermissionResult(boolean waiting) {
 
     }
